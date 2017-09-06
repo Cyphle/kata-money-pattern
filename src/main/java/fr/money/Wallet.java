6ab89722d0a9b2288e778d4d1e8792a90e778467
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-06 16:31:35 +0200

    Have 10 when adding 10

--- src/main/java/fr/money/Wallet.java
+++ src/main/java/fr/money/Wallet.java
@@ -1,9 +1,20 @@
 package fr.money;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Wallet {
+  private BigDecimal amount;
+
+  public Wallet() {
+    amount = new BigDecimal(0, MathContext.DECIMAL64);
+  }
+
   public BigDecimal getAmount() {
-    return new BigDecimal(0);
+    return amount;
+  }
+
+  public void add(BigDecimal amountToAdd) {
+    amount = amount.add(amountToAdd);
   }
 }
--- src/test/java/fr/money/WalletTest.java
+++ src/test/java/fr/money/WalletTest.java
@@ -12,4 +12,11 @@ public class WalletTest {
     Wallet wallet = new Wallet();
     assertThat(wallet.getAmount()).isEqualTo(new BigDecimal(0));
   }
+
+  @Test
+  public void should_have_10_when_adding_10_in_wallet() throws Exception {
+    Wallet wallet = new Wallet();
+    wallet.add(new BigDecimal(10));
+    assertThat(wallet.getAmount()).isEqualTo(new BigDecimal(10));
+  }
 }
